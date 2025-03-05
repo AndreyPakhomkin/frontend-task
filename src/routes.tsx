4bdf4e78b29f9fe563187ec39cdcec8dd0414a3c
Andrey Pakhomkin <yoyodrews@yandex.ru>
2025-03-05 16:21:51 +0300

    Инициализация проекта, зависимости, первичная настройка

---
+++ src/routes.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const AppRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRoutes;
